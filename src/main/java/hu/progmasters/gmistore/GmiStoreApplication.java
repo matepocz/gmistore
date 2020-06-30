80c334c9f6b01fafb212d82c5b5078c9a8a0a1d2
pocz.mate <mate.pocz@gmail.com>
2020-06-30 22:16:08 +0200

    Initial commit

---
+++ src/main/java/hu/progmasters/gmistore/GmiStoreApplication.java
@@ -0,0 +1,13 @@
+package hu.progmasters.gmistore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmiStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmiStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hu/progmasters/gmistore/GmiStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.progmasters.gmistore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmiStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

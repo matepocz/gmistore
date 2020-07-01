9049a4aa08934c1e7d0cd4e33209af8a5bd93e2e
pocz.mate <mate.pocz@gmail.com>
2020-07-01 22:43:40 +0200

    impl getAllProducts

--- src/main/java/hu/progmasters/gmistore/controller/ProductController.java
+++ src/main/java/hu/progmasters/gmistore/controller/ProductController.java
@@ -1,6 +1,6 @@
 package hu.progmasters.gmistore.controller;
 
-import hu.progmasters.gmistore.model.Product;
+import hu.progmasters.gmistore.dto.ProductDto;
 import hu.progmasters.gmistore.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,19 +9,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/product")
+@RequestMapping("/api/products/")
 public class ProductController {
 
     @Autowired
     private ProductService productService;
 
     @GetMapping("/all")
-    public ResponseEntity<List<Product>> products() {
-        List<Product> products = new ArrayList<>();
+    public ResponseEntity<List<ProductDto>> products() {
+        List<ProductDto> products = productService.getAllProducts();
         return new ResponseEntity<>(products, HttpStatus.OK);
     }
 }

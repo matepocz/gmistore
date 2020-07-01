4aea4ff3b734da56ed9a41158b3ee69655371ce5
pocz.mate <mate.pocz@gmail.com>
2020-07-01 23:07:39 +0200

    add getProductById

--- src/main/java/hu/progmasters/gmistore/service/ProductService.java
+++ src/main/java/hu/progmasters/gmistore/service/ProductService.java
@@ -1,8 +1,10 @@
 package hu.progmasters.gmistore.service;
 
+import hu.progmasters.gmistore.ProductNotFoundException;
 import hu.progmasters.gmistore.dto.ProductDto;
 import hu.progmasters.gmistore.model.Product;
 import hu.progmasters.gmistore.repository.ProductRepository;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,6 +15,8 @@ import java.util.stream.Collectors;
 @Service
 @Transactional
 public class ProductService {
+
+    private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(ProductService.class);
 
     @Autowired
     private ProductRepository productRepository;
@@ -25,6 +29,18 @@ public class ProductService {
     public List<ProductDto> getAllProducts() {
         List<Product> allProduct = productRepository.findAll();
         return allProduct.stream().map(this::mapProductToProductDto).collect(Collectors.toList());
+    }
+
+    /**
+     * Get a product with the specified id
+     *
+     * @param id Product's unique id
+     * @return A ProductDto, if not found throws ProductNotFoundException
+     */
+    public ProductDto getProductById(Long id) {
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new ProductNotFoundException("Product not found"));
+        return mapProductToProductDto(product);
     }
 
     private ProductDto mapProductToProductDto(Product product) {

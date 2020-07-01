f74ab0706b03fb237f245b47fb3359f947eba0dd
pocz.mate <mate.pocz@gmail.com>
2020-07-01 15:25:10 +0200

    impl CustomerService

---
+++ src/main/java/hu/progmasters/gmistore/service/CustomerService.java
@@ -0,0 +1,10 @@
+package hu.progmasters.gmistore.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class CustomerService {
+
+}

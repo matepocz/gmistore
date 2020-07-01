6e23cf9b988604b274bbbaf52f713036b3975828
pocz.mate <mate.pocz@gmail.com>
2020-07-01 15:22:23 +0200

    impl ProductService

---
+++ src/main/java/hu/progmasters/gmistore/service/ProductService.java
@@ -0,0 +1,9 @@
+package hu.progmasters.gmistore.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class ProductService {
+}

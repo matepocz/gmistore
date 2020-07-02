7d16acae23212634b8a93913ee0729350c9d8694
pocz.mate <mate.pocz@gmail.com>
2020-07-03 00:17:34 +0200

    lombok-ize ProductDto

--- src/main/java/hu/progmasters/gmistore/dto/ProductDto.java
+++ src/main/java/hu/progmasters/gmistore/dto/ProductDto.java
@@ -1,13 +1,14 @@
 package hu.progmasters.gmistore.dto;
 
-import hu.progmasters.gmistore.model.Product;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class ProductDto {
 
     private long id;
@@ -22,28 +23,4 @@ public class ProductDto {
     private List<Integer> ratings;
     private double averageRating;
     private boolean active;
-
-    public ProductDto() {
-    }
-
-    public ProductDto(Product product) {
-        this.id = product.getId();
-        this.name = product.getName();
-        this.description = product.getDescription();
-        if (product.getCategory() != null) {
-            this.category = product.getCategory().getDisplayName();
-        }
-        if (product.getPictureUrl() != null) {
-            this.pictureUrl = product.getPictureUrl();
-        }
-        this.price = product.getPrice();
-        this.discount = product.getDiscount();
-        this.warrantyMonths = product.getWarrantyMonths();
-        this.quantityAvailable = product.getQuantityAvailable();
-        if(product.getRatings() != null) {
-            this.ratings = product.getRatings();
-        }
-        this.averageRating = product.getAverageRating();
-        this.active = product.isActive();
-    }
 }

05184a2d1afa1a4dac3d81012f44fc40ef3e09d5
pocz.mate <mate.pocz@gmail.com>
2020-07-02 22:11:32 +0200

    add category field + getter

--- src/main/java/hu/progmasters/gmistore/enums/Category.java
+++ src/main/java/hu/progmasters/gmistore/enums/Category.java
@@ -1,7 +1,15 @@
 package hu.progmasters.gmistore.enums;
 
 public enum Category {
-    TV,
-    PHONE,
-    FRIDGE,
+    TV("Tv"), PHONE("Phone"), FRIDGE("Fridge"), COMPUTER("Computer"), PRINTER("Printer");
+
+    private String category;
+
+    Category(String category) {
+        this.category = category;
+    }
+
+    public String getCategory() {
+        return category;
+    }
 }

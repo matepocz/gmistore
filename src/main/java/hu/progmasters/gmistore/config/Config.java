abf9e59fe7d1d374d6d5ab32fded4894a850a523
pocz.mate <mate.pocz@gmail.com>
2020-07-04 00:18:35 +0200

    add Config.java

---
+++ src/main/java/hu/progmasters/gmistore/config/Config.java
@@ -0,0 +1,34 @@
+package hu.progmasters.gmistore.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties
+public class Config {
+
+    @Value("${cloudinary.cloudName}")
+    private String cloudinaryCloudName;
+
+    @Value("${cloudinary.apiKey}")
+    private String cloudinaryApiKey;
+
+    @Value("${cloudinary.apiSecret}")
+    private String cloudinaryApiSecret;
+
+    @Bean
+    public String cloudinaryCloudName() {
+        return cloudinaryCloudName;
+    }
+
+    @Bean
+    public String cloudinaryApiKey() {
+        return cloudinaryApiKey;
+    }
+
+    public String cloudinaryApiSecret() {
+        return cloudinaryApiSecret;
+    }
+}

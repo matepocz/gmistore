8616db5f0ef67fae94055434d6c74e4136c2327f
pocz.mate <mate.pocz@gmail.com>
2020-07-04 22:04:24 +0200

    improve Customer

--- src/main/java/hu/progmasters/gmistore/model/Customer.java
+++ src/main/java/hu/progmasters/gmistore/model/Customer.java
@@ -1,11 +1,16 @@
 package hu.progmasters.gmistore.model;
 
+import hu.progmasters.gmistore.enums.Role;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.lang.NonNull;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @NoArgsConstructor
@@ -18,18 +23,39 @@ public class Customer {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
-    @NonNull
+    @NotNull
+    @Column(columnDefinition = "varchar(100)", unique = true)
     private String username;
+
+    @Column(columnDefinition = "varchar(100)")
+    private String lastName;
+
+    @Column(columnDefinition = "varchar(100)")
+    private String firstName;
 
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "address_id", referencedColumnName = "id")
     private Address address;
 
-    @NonNull
+    @NotNull
     private String password;
 
-    @NonNull
+    @NotNull
+    @Email
+    @Column(unique = true)
     private String email;
 
     private String phoneNumber;
+
+    @Enumerated(EnumType.STRING)
+    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
+    @CollectionTable(name = "roles")
+    @Column(name = "roles")
+    private List<Role> roles = new ArrayList<>();
+
+    private Instant registered;
+
+    @Column(columnDefinition = "boolean default false")
+    private boolean active;
+
 }

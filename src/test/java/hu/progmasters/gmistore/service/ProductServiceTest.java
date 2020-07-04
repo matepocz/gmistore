9217f69082cda79f7fca30bcd5b9ffc06144babb
pocz.mate <mate.pocz@gmail.com>
2020-07-04 22:03:39 +0200

    add ProductServiceTest

---
+++ src/test/java/hu/progmasters/gmistore/service/ProductServiceTest.java
@@ -0,0 +1,16 @@
+package hu.progmasters.gmistore.service;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ProductServiceTest {
+
+    @Autowired
+    private ProductService productService;
+
+    @Test
+    public void test() {
+    }
+}

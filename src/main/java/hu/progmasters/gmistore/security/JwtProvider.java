5bf7b84da2ea5fe23994fbb41fc3df533f5a07cd
pocz.mate <mate.pocz@gmail.com>
2020-07-06 14:34:56 +0200

    impl keystore

--- src/main/java/hu/progmasters/gmistore/security/JwtProvider.java
+++ src/main/java/hu/progmasters/gmistore/security/JwtProvider.java
@@ -1,42 +1,71 @@
 package hu.progmasters.gmistore.security;
 
+import hu.progmasters.gmistore.exception.KeyException;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
-import java.security.Key;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.*;
+import java.security.cert.CertificateException;
 
 @Service
 public class JwtProvider {
 
-    private Key key;
+    @Value("${keyStore.password}")
+    private String keyStorePassword;
+
+    private KeyStore keyStore;
 
     @PostConstruct
     public void init() {
-        key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+        try {
+            keyStore = KeyStore.getInstance("JKS");
+            InputStream resourceAsStream = getClass().getResourceAsStream("/gmistore.jks");
+            keyStore.load(resourceAsStream, keyStorePassword.toCharArray());
+        } catch (KeyStoreException | CertificateException | NoSuchAlgorithmException | IOException e) {
+            throw new KeyException("An error occurred while loading keystore!");
+        }
+
     }
 
     public String generateToken(Authentication authentication) {
         User principal = (User) authentication.getPrincipal();
         return Jwts.builder()
                 .setSubject(principal.getUsername())
-                .signWith(key)
+                .signWith(getPrivateKey())
                 .compact();
     }
 
+    private PrivateKey getPrivateKey() {
+        try {
+            return (PrivateKey) keyStore.getKey("gmistore", keyStorePassword.toCharArray());
+        } catch (KeyStoreException | NoSuchAlgorithmException | UnrecoverableKeyException e) {
+             throw new KeyException("An error occurred while retrieving private key from the keystore!");
+        }
+    }
+
     public boolean validateToken(String jwt) {
-        Jwts.parser().setSigningKey(key).parseClaimsJws(jwt);
+        Jwts.parser().setSigningKey(getPublicKey()).parseClaimsJws(jwt);
         return true;
+    }
+
+    private PublicKey getPublicKey() {
+        try {
+            return keyStore.getCertificate("gmistore").getPublicKey();
+        } catch (KeyStoreException e) {
+            throw new KeyException("An error occurred while retrieving public key from the keystore!");
+        }
     }
 
     public String getUsernameFromJwt(String token) {
         Claims claims = Jwts.parser()
-                .setSigningKey(key)
+                .setSigningKey(getPublicKey())
                 .parseClaimsJws(token)
                 .getBody();
         return claims.getSubject();

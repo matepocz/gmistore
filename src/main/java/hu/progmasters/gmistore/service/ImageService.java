3c635f3dcad2bb1d65ced51777f97b446a7d705d
pocz.mate <mate.pocz@gmail.com>
2020-07-07 13:06:08 +0200

    improve security

--- src/main/java/hu/progmasters/gmistore/service/ImageService.java
+++ src/main/java/hu/progmasters/gmistore/service/ImageService.java
@@ -2,6 +2,7 @@ package hu.progmasters.gmistore.service;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,14 +14,15 @@ import java.util.Map;
 @Transactional
 public class ImageService {
 
-    Cloudinary cloudinary = new Cloudinary(ObjectUtils.asMap(
-            "cloud_name", "des0anwfu",
-            "api_key", "797962421181434",
-            "api_secret", "luBbkMUxj-XblHRnnOtFl11u5fI"));
+    private final Cloudinary cloudinaryConfig;
 
+    @Autowired
+    public ImageService(Cloudinary cloudinaryConfig) {
+        this.cloudinaryConfig = cloudinaryConfig;
+    }
 
     public String[] uploadImage(MultipartFile imageToUpload) throws IOException {
-        Map uploadResult = cloudinary.uploader().upload(imageToUpload.getBytes(), ObjectUtils.emptyMap());
+        Map uploadResult = cloudinaryConfig.uploader().upload(imageToUpload.getBytes(), ObjectUtils.emptyMap());
 
         String[] result = new String[2];
 

20bb2d1450de72c5baaa90ca13d3918525dbf41b
pocz.mate <mate.pocz@gmail.com>
2020-07-09 15:05:24 +0200

    delete extra method

--- src/main/java/hu/progmasters/gmistore/repository/OrderRepository.java
+++ src/main/java/hu/progmasters/gmistore/repository/OrderRepository.java
@@ -1,6 +1,7 @@
 package hu.progmasters.gmistore.repository;
 
 import hu.progmasters.gmistore.model.Order;
+import hu.progmasters.gmistore.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,5 @@ import java.util.Optional;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    Optional<Order> findOrderById(int id);
+
 }

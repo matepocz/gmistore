98fced4e99bc7b1ba64ea8038a06bd13aaa7576c
pocz.mate <mate.pocz@gmail.com>
2020-07-10 23:31:17 +0200

    fix Repo

--- src/main/java/hu/progmasters/gmistore/repository/AddressRepository.java
+++ src/main/java/hu/progmasters/gmistore/repository/AddressRepository.java
@@ -2,6 +2,8 @@ package hu.progmasters.gmistore.repository;
 
 import hu.progmasters.gmistore.model.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
 }

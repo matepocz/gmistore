84808666fc93195845d8f4484c8845a796a07e41
pocz.mate <mate.pocz@gmail.com>
2020-07-11 23:41:08 +0200

    impl ConfirmAccountResponse

---
+++ src/main/java/hu/progmasters/gmistore/service/ConfirmAccountResponse.java
@@ -0,0 +1,12 @@
+package hu.progmasters.gmistore.service;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ConfirmAccountResponse {
+
+    private String result;
+    private String email;
+}

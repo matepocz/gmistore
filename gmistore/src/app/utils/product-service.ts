1eadf43b0a1977d10b99df6279464ec2d1ad8e8f
pocz.mate <mate.pocz@gmail.com>
2020-07-15 14:43:16 +0200

    update url

--- gmistore/src/app/utils/product-service.ts
+++ gmistore/src/app/utils/product-service.ts
@@ -1,18 +1,19 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 import {Product} from "../product/product";
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  private productsUrl = environment.apiUrl + 'products/';
 
-  private baseUrl = 'http://localhost:8080/api/products/';
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
   getProduct(id: number): Observable<Product> {
-    return this.http.get<Product>(`${this.baseUrl}get/${id}`);
+    return this.http.get<Product>(this.productsUrl + 'get/' + id);
   }
 }

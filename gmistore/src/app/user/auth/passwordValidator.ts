5d53d4fd2e11631810f4a1ba0cfddffc578c01d7
palasti_imre <palastiimre@gmail.com>
2020-07-17 16:45:54 +0200

    add passwordValidator.ts

---
+++ gmistore/src/app/user/auth/passwordValidator.ts
@@ -0,0 +1,21 @@
+import {FormGroup} from "@angular/forms";
+
+export function ComparePassword(
+  password: string,
+  confirmPassword: string) {
+
+  return (formGroup: FormGroup) => {
+    const control = formGroup.controls[password];
+    const matchingControl = formGroup.controls[confirmPassword];
+
+    if (matchingControl.errors && !matchingControl.errors.mustMatch) {
+      return;
+    }
+
+    if (control.value !== matchingControl.value) {
+      matchingControl.setErrors({ mustMatch: true });
+    } else {
+      matchingControl.setErrors(null);
+    }
+  };
+}

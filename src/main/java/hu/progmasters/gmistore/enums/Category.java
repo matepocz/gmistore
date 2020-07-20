bdcb17822b6f09da872c5dd28f746f3116f302a9
pocz.mate <mate.pocz@gmail.com>
2020-07-20 14:58:19 +0200

    add more categories

--- src/main/java/hu/progmasters/gmistore/enums/Category.java
+++ src/main/java/hu/progmasters/gmistore/enums/Category.java
@@ -3,11 +3,17 @@ package hu.progmasters.gmistore.enums;
 import lombok.Getter;
 
 public enum Category {
-    TV("Tv"), PHONE("Phone"), FRIDGE("Fridge"),
-    COMPUTER("Computer"), PRINTER("Printer");
+    TV("Tv"), MONITOR("Monitor"), PHONE("Mobiltelefon"),
+    FRIDGE("Hűtő"), FREEZER("Fagyasztó"), COMPUTER("Számítógép"),
+    LAPTOP("Laptop"), TABLET("Tablet"), PRINTER("Nyomtató"),
+    NAVIGATION("Navigáció"), CAMERA("Fényképezőgép"), KEYBOARD("Billenytűzet"),
+    MOUSE("Egér"), WASHING_MACHINE("Mosógép"), DRYER("Szárítógép"),
+    PROCESSOR("Processzor"), RAM("Memória"), HDD("Merevlemez"),
+    SSD("SSD"), MOTHERBOARD("Alaplap"), VGA("Videókártya"),
+    UNKNOWN("Nem kategórizált");
 
     @Getter
-    private String displayName;
+    private final String displayName;
 
     Category(String displayName) {
         this.displayName = displayName;

d8a516fd113900660cc7dccbf2e4a836be2e1f1e
pocz.mate <mate.pocz@gmail.com>
2020-07-21 08:23:04 +0200

    impl getUserData

--- gmistore/src/app/service/user.service.ts
+++ gmistore/src/app/service/user.service.ts
@@ -1,17 +1,21 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../../environments/environment";
+import {Observable} from "rxjs";
+import {User} from "../models/user";
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  apiUrl = environment.apiUrl;
+  apiUrl = environment.apiUrl + 'api/';
 
-  constructor(private http: HttpClient) { }
+  constructor(private httpClient: HttpClient) {
+  }
 
-
-
+  getUser(): Observable<User> {
+    return this.httpClient.get<User>(this.apiUrl + 'user/my-account');
+  }
 }
 

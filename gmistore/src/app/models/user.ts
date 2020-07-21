42a3e5e96cc9676e28beb6d6e01f5ad7df1fae6c
pocz.mate <mate.pocz@gmail.com>
2020-07-21 08:22:49 +0200

    change address type

--- gmistore/src/app/models/user.ts
+++ gmistore/src/app/models/user.ts
@@ -1,17 +1,18 @@
 import {OrderModel} from "./orderModel";
+import {AddressModel} from "./addressModel";
 
 export interface User {
   id: number;
-  username:string;
+  username: string;
   lastName: string;
   firstName: string;
-  address: string;
+  address: AddressModel;
   email: string;
   phoneNumber: string;
-  roles:Array<string>;
-  registered: any;
+  roles: Array<string>;
+  registered: Date;
   active: boolean;
-  orderList:Array<OrderModel>;
+  orderList: Array<OrderModel>;
 
 }
 

eb61ff9f678adbf5f86d228398916571eaf15cd0
pocz.mate <mate.pocz@gmail.com>
2020-07-28 11:10:53 +0200

    improve edit product

--- gmistore/src/app/product/product-edit/product-edit.component.ts
+++ gmistore/src/app/product/product-edit/product-edit.component.ts
@@ -16,12 +16,10 @@ export class ProductEditComponent implements OnInit {
   loading = false;
   selectedFiles: FileList;
   productPictures: string[];
-  categories: Map<String, String>;
+  categories: Map<string, string>;
   productForm: FormGroup;
 
-  productId: number;
-  productSlug: string;
-  productAddedBy: string;
+  currentCategory: string;
 
   constructor(private productService: ProductService, private formBuilder: FormBuilder,
               private route: ActivatedRoute, private router: Router) {
@@ -29,55 +27,63 @@ export class ProductEditComponent implements OnInit {
 
   ngOnInit(): void {
     this.loading = true;
-    this.categories = new Map<String, String>();
+    this.categories = new Map<string, string>();
     this.productService.getProductCategories().subscribe(
       (data) => {
         for (let value in data) {
-          this.categories.set(value, data[value]);
+          this.categories.set(value, data[value].toString());
         }
       }, (error) => {
-        console.warn(error);
-      }
-    );
-    let slug = this.route.snapshot.params['slug'];
-    this.productService.getProductBySlug(slug).subscribe((data) => {
-      this.product = data;
-    }, (error) => {
-      console.log(error);
-    }, () => {
-      this.productId = this.product.id;
-      this.productSlug = this.product.slug;
-      this.productPictures = this.product.pictures;
-      this.productAddedBy = this.product.addedBy;
-      this.productForm = this.formBuilder.group({
-        'name': [this.product.name],
-        'productCode': [this.product.productCode],
-        'description': [this.product.description, Validators.compose(
-          [Validators.required, Validators.minLength(10)])],
-        'category': [this.product.category, Validators.required],
-        'pictureUrl': [this.product.pictureUrl],
-        'pictures': [this.product.pictures],
-        'price': [this.product.price, Validators.compose([Validators.required, Validators.min(0)])],
-        'discount': [this.product.discount, Validators.compose(
-          [Validators.required, Validators.min(0), Validators.max(100)])],
-        'warrantyMonths': [this.product.warrantyMonths, Validators.compose(
-          [Validators.required, Validators.min(0), Validators.max(360)])],
-        'quantityAvailable': [this.product.quantityAvailable, Validators.compose([Validators.required, Validators.min(0)])],
-        'active': [this.product.active],
+        console.warn(error)
       });
-      this.loading = false;
-    });
+
+    let slug = this.route.snapshot.params['slug'];
+    this.productService.getProductBySlug(slug).subscribe(
+      (data) => {
+        this.product = data;
+      }, (error) => {
+        console.log(error);
+      }, () => {
+        for (let [key, value] of this.categories) {
+          if (value === this.product.category) {
+            this.currentCategory = key;
+          }
+        }
+
+        this.productPictures = this.product.pictures;
+
+        this.productForm = this.formBuilder.group({
+          id: [this.product.id],
+          name: [this.product.name],
+          slug: [this.product.slug],
+          productCode: [this.product.productCode],
+          description: [this.product.description, Validators.compose(
+            [Validators.required, Validators.minLength(10)])],
+          category: [this.product.category, Validators.required],
+          pictureUrl: [this.product.pictureUrl],
+          pictures: [this.product.pictures],
+          price: [this.product.price, Validators.compose([Validators.required, Validators.min(0)])],
+          discount: [this.product.discount, Validators.compose(
+            [Validators.required, Validators.min(0), Validators.max(100)])],
+          warrantyMonths: [this.product.warrantyMonths, Validators.compose(
+            [Validators.required, Validators.min(0), Validators.max(360)])],
+          quantityAvailable: [this.product.quantityAvailable, Validators.compose([Validators.required, Validators.min(0)])],
+          active: [this.product.active],
+          addedBy: [this.product.addedBy]
+        });
+        this.loading = false;
+      });
   }
 
   onSubmit() {
     this.loading = true;
+    debugger
     this.product = this.productForm.value;
-    this.product.id = this.productId;
-    this.product.slug = this.productSlug;
-    this.product.addedBy = this.productAddedBy;
+
     this.productService.updateProduct(this.product, this.product.slug).subscribe(
       (data) => {
-      }, (error) => {
+      },
+      (error) => {
         errorHandler(error, this.productForm);
       }, () => {
         this.router.navigate(['/product', this.product.slug]);

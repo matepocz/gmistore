8c89248e3d77414179e56484ef089da6ef070f43
pocz.mate <mate.pocz@gmail.com>
2020-08-04 07:39:48 +0200

    fix missing role

--- src/main/java/hu/progmasters/gmistore/enums/Role.java
+++ src/main/java/hu/progmasters/gmistore/enums/Role.java
@@ -5,7 +5,8 @@ import lombok.Getter;
 @Getter
 public enum Role {
     ROLE_USER("user"),
-    ROLE_ADMIN("admin");
+    ROLE_ADMIN("admin"),
+    ROLE_SELLER("seller");
 
     private String displayName;
 

056385ed6144c6ec04b71cfbe3529083748f8565
pocz.mate <mate.pocz@gmail.com>
2020-08-14 07:41:32 +0200

    impl new DTO for products in cart

--- src/main/java/hu/progmasters/gmistore/dto/CartItemDto.java
+++ src/main/java/hu/progmasters/gmistore/dto/CartItemDto.java
@@ -11,34 +11,26 @@ import lombok.Setter;
 @NoArgsConstructor
 public class CartItemDto {
     private Long id;
-    private ProductDto product;
+    private ProductInCartDetails product;
     private Integer count;
 
     public CartItemDto(CartItem cartItem) {
         this.id = cartItem.getId();
-        this.product = mapProductToProductDto(cartItem.getProduct());
+        this.product = mapProductToDetails(cartItem.getProduct());
         this.count = cartItem.getCount();
     }
 
-    private ProductDto mapProductToProductDto(Product product) {
-        ProductDto productDto = new ProductDto();
+    private ProductInCartDetails mapProductToDetails(Product product) {
+        ProductInCartDetails productDto = new ProductInCartDetails();
         productDto.setId(product.getId());
         productDto.setName(product.getName());
         productDto.setProductCode(product.getProductCode());
         productDto.setSlug(product.getSlug());
-        productDto.setDescription(product.getDescription());
-        productDto.setCategory(product.getCategory().getDisplayName());
         productDto.setPictureUrl(product.getPictureUrl());
-        productDto.setPictures(product.getPictures());
         productDto.setPrice(product.getPrice());
         productDto.setDiscount(product.getDiscount());
         productDto.setWarrantyMonths(product.getWarrantyMonths());
         productDto.setQuantityAvailable(product.getInventory().getQuantityAvailable());
-        productDto.setQuantitySold(product.getInventory().getQuantitySold());
-        productDto.setRatings(product.getRatings());
-        productDto.setAverageRating(product.getAverageRating());
-        productDto.setActive(product.isActive());
-        productDto.setAddedBy(product.getAddedBy());
         return productDto;
     }
 }
---
+++ src/main/java/hu/progmasters/gmistore/dto/ProductInCartDetails.java
@@ -0,0 +1,20 @@
+package hu.progmasters.gmistore.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProductInCartDetails {
+
+    private Long id;
+    private String name;
+    private String productCode;
+    private String slug;
+    private String pictureUrl;
+    private Double price;
+    private Integer discount;
+    private Integer warrantyMonths;
+    private Integer quantityAvailable;
+
+}

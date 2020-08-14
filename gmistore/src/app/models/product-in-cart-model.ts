f2ea5b159c44c90577196ed2bcf91ad4e9e88229
pocz.mate <mate.pocz@gmail.com>
2020-08-14 07:43:18 +0200

    impl new model for products in cart

---
+++ gmistore/src/app/models/product-in-cart-model.ts
@@ -0,0 +1,11 @@
+export interface ProductInCartModel {
+  id: number;
+  name: string;
+  productCode: string;
+  slug: string;
+  pictureUrl: string;
+  price: number;
+  discount: number;
+  warrantyMonths: number;
+  quantityAvailable: number;
+}

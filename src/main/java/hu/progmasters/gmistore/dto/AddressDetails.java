ef372cd1a88ab3e4ee2be874bd4e42046c890cb8
pocz.mate <mate.pocz@gmail.com>
2020-08-30 22:36:35 +0200

    impl AddressDetails

---
+++ src/main/java/hu/progmasters/gmistore/dto/AddressDetails.java
@@ -0,0 +1,31 @@
+package hu.progmasters.gmistore.dto;
+
+import hu.progmasters.gmistore.model.Address;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class AddressDetails {
+    private Long id;
+    private String city;
+    private String street;
+    private int number;
+    private int floor;
+    private int door;
+    private String country;
+    private String postcode;
+
+    public AddressDetails(Address address) {
+        this.id = address.getId();
+        this.city = address.getCity();
+        this.street = address.getStreet();
+        this.number = address.getNumber();
+        this.floor = address.getFloor();
+        this.door = address.getDoor();
+        this.country = address.getCountry();
+        this.postcode = address.getPostcode();
+    }
+}

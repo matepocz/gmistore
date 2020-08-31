026d34f9f1e1a1323c22707b0ef25da06a2bca32
pocz.mate <mate.pocz@gmail.com>
2020-08-31 22:02:25 +0200

    add getLookupByKey

--- src/main/java/hu/progmasters/gmistore/service/LookupService.java
+++ src/main/java/hu/progmasters/gmistore/service/LookupService.java
@@ -1,5 +1,7 @@
 package hu.progmasters.gmistore.service;
 
+import hu.progmasters.gmistore.enums.DomainType;
+import hu.progmasters.gmistore.model.LookupEntity;
 import hu.progmasters.gmistore.repository.LookupRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,5 +17,9 @@ public class LookupService {
     @Autowired
     public LookupService(LookupRepository lookupRepository) {
         this.lookupRepository = lookupRepository;
+    }
+
+    public LookupEntity getPaymentMethodByKey(String key) {
+        return lookupRepository.findByDomainTypeAndLookupKey(DomainType.PAYMENT_METHOD, key);
     }
 }

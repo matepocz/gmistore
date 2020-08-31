75498e3e32d2051613f98a1b3b6a6619519fad68
pocz.mate <mate.pocz@gmail.com>
2020-08-31 22:03:09 +0200

    add createOrder endpoint

--- src/main/java/hu/progmasters/gmistore/controller/OrderController.java
+++ src/main/java/hu/progmasters/gmistore/controller/OrderController.java
@@ -2,16 +2,19 @@ package hu.progmasters.gmistore.controller;
 
 import hu.progmasters.gmistore.dto.CustomerDetails;
 import hu.progmasters.gmistore.service.OrderService;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("/api/orders")
 public class OrderController {
+
+    private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(OrderController.class);
 
     private final OrderService orderService;
 
@@ -22,9 +25,16 @@ public class OrderController {
 
     @GetMapping("/customer-details")
     public ResponseEntity<CustomerDetails> getCustomerDetails() {
+        LOGGER.debug("Customer details requested!");
         CustomerDetails customerDetails = orderService.getCustomerDetails();
         return customerDetails != null ?
-                new ResponseEntity<>(customerDetails, HttpStatus.OK):
+                new ResponseEntity<>(customerDetails, HttpStatus.OK) :
                 new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createOrder(@RequestBody CustomerDetails customerDetails, HttpSession session) {
+        orderService.createOrder(customerDetails, session);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }

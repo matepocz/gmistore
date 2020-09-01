f97384c5442e190906cc5fe94c802355bbdb1192
pocz.mate <mate.pocz@gmail.com>
2020-09-01 23:21:14 +0200

    adjust for new DTO

--- gmistore/src/app/service/order.service.ts
+++ gmistore/src/app/service/order.service.ts
@@ -3,6 +3,7 @@ import {HttpClient} from '@angular/common/http';
 import {environment} from "../../environments/environment";
 import {Observable} from "rxjs";
 import {CustomerDetailsModel} from "../models/customer-details.model";
+import {OrderRequestModel} from "../models/order-request.model";
 
 @Injectable({
   providedIn: 'root'
@@ -18,7 +19,7 @@ export class OrderService {
     return this.httpClient.get<CustomerDetailsModel>(this.ordersUrl + '/customer-details');
   }
 
-  createOrder(data: CustomerDetailsModel): Observable<any> {
+  createOrder(data: OrderRequestModel): Observable<any> {
     return this.httpClient.post(this.ordersUrl, data);
   }
 }

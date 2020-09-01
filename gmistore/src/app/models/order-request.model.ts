6b5e1a48ba9e3b881a13603c5a139921febdecb0
pocz.mate <mate.pocz@gmail.com>
2020-09-01 22:48:29 +0200

    impl new DTOs

---
+++ gmistore/src/app/models/order-request.model.ts
@@ -0,0 +1,11 @@
+import {AddressModel} from "./address-model";
+
+export interface OrderRequestModel {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phoneNumber: string;
+  shippingAddress: AddressModel;
+  billingAddress: AddressModel;
+  paymentMethod: string;
+}

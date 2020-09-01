f8a7f1183918b62e686490a3cf7275a364feef52
pocz.mate <mate.pocz@gmail.com>
2020-09-01 23:20:47 +0200

    impl findByProduct

--- src/main/java/hu/progmasters/gmistore/repository/InventoryRepository.java
+++ src/main/java/hu/progmasters/gmistore/repository/InventoryRepository.java
@@ -1,10 +1,13 @@
 package hu.progmasters.gmistore.repository;
 
 import hu.progmasters.gmistore.model.Inventory;
+import hu.progmasters.gmistore.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
-
+    Optional<Inventory> findByProduct(Product product);
 }

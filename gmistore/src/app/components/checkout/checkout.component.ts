12c8d10ba70626ce06e2223069389864d0239fa5
pocz.mate <mate.pocz@gmail.com>
2020-09-02 22:21:03 +0200

    finish checkout

--- gmistore/src/app/components/checkout/checkout.component.ts
+++ gmistore/src/app/components/checkout/checkout.component.ts
@@ -9,6 +9,9 @@ import {CartModel} from "../../models/cart-model";
 import {Title} from "@angular/platform-browser";
 import {SideNavComponent} from "../side-nav/side-nav.component";
 import {OrderRequestModel} from "../../models/order-request.model";
+import {Router} from "@angular/router";
+import {PaymentMethodDetailsModel} from "../../models/payment-method-details.model";
+import {errorHandler} from "../../utils/error-handler";
 
 @Component({
   selector: 'app-checkout',
@@ -24,6 +27,8 @@ export class CheckoutComponent implements OnInit, OnDestroy {
   cartDetails: CartModel;
   customerDetails: CustomerDetailsModel;
   orderRequest: OrderRequestModel;
+  paymentMethods: Array<PaymentMethodDetailsModel>;
+  chosenPaymentMethod: PaymentMethodDetailsModel;
 
   detailsForm: FormGroup;
   shippingAddressForm: FormGroup;
@@ -32,15 +37,31 @@ export class CheckoutComponent implements OnInit, OnDestroy {
   customerDetailsSub: Subscription;
   cartSub: Subscription;
   createOrderSub: Subscription;
+  paymentMethodsSub: Subscription;
 
   constructor(private formBuilder: FormBuilder, private cartService: CartService,
               private authService: AuthService, private orderService: OrderService,
-              private titleService: Title, private sideNav: SideNavComponent) {
+              private titleService: Title, private sideNav: SideNavComponent, private router: Router) {
   }
 
   ngOnInit(): void {
     this.titleService.setTitle("Kosár véglegesítése - GMI Store")
     this.authenticatedUser = this.authService.isAuthenticated();
+
+    this.cartSub = this.cartService.getCart().subscribe(
+      (response) => {
+        this.cartDetails = response;
+        if (this.cartDetails.cartItems.length <= 0) {
+          this.router.navigate(['/cart']);
+        }
+      }, (error) => {
+        console.log(error);
+        this.loading = false;
+      },
+      () => {
+        this.loading = false;
+      }
+    );
 
     this.detailsForm = this.formBuilder.group({
       firstName: [null],
@@ -54,7 +75,7 @@ export class CheckoutComponent implements OnInit, OnDestroy {
       city: [null,
         Validators.compose([Validators.required, Validators.minLength(3)])],
       street: [null,
-        Validators.compose([Validators.required, Validators.minLength(3)])],
+        Validators.compose([Validators.required, Validators.minLength(2)])],
       number: [null,
         Validators.compose([Validators.required, Validators.min(1)])],
       floor: [null],
@@ -67,14 +88,30 @@ export class CheckoutComponent implements OnInit, OnDestroy {
 
     this.billingAddressForm = this.formBuilder.group({
       id: [null],
-      city: [null],
-      street: [null],
-      number: [null],
+      city: [null,
+        Validators.compose([Validators.required, Validators.minLength(3)])],
+      street: [null,
+        Validators.compose([Validators.required, Validators.minLength(2)])],
+      number: [null,
+        Validators.compose([Validators.required, Validators.min(1)])],
       floor: [null],
       door: [null],
-      country: [null],
-      postcode: [null]
+      country: [null,
+        Validators.compose([Validators.required, Validators.minLength(3)])],
+      postcode: [null,
+        Validators.compose([Validators.required, Validators.minLength(3)])]
     });
+
+    this.paymentMethodsSub = this.orderService.getPaymentMethods().subscribe(
+      (response) => {
+        this.paymentMethods = response;
+        if (this.paymentMethods.length > 0) {
+          this.chosenPaymentMethod = this.paymentMethods[0];
+        }
+      }, (error) => {
+        console.log(error);
+      }
+    )
 
     if (this.authenticatedUser) {
       this.customerDetailsSub = this.orderService.getCustomerDetails().subscribe(
@@ -85,50 +122,47 @@ export class CheckoutComponent implements OnInit, OnDestroy {
           console.log(error);
         },
         () => {
-          this.detailsForm.patchValue({
-            firstName: this.customerDetails.firstName,
-            lastName: this.customerDetails.lastName,
-            email: this.customerDetails.email,
-            phoneNumber: this.customerDetails.phoneNumber
-          });
-
-          this.shippingAddressForm.patchValue({
-            id: this.customerDetails.shippingAddress.id,
-            city: this.customerDetails.shippingAddress.city,
-            street: this.customerDetails.shippingAddress.street,
-            number: this.customerDetails.shippingAddress.number,
-            floor: this.customerDetails.shippingAddress.floor,
-            door: this.customerDetails.shippingAddress.door,
-            country: this.customerDetails.shippingAddress.country,
-            postcode: this.customerDetails.shippingAddress.postcode
-          });
-
-          this.billingAddressForm.patchValue({
-            id: this.customerDetails.billingAddress.id,
-            city: this.customerDetails.billingAddress.city,
-            street: this.customerDetails.billingAddress.street,
-            number: this.customerDetails.billingAddress.number,
-            floor: this.customerDetails.billingAddress.floor,
-            door: this.customerDetails.billingAddress.door,
-            country: this.customerDetails.billingAddress.country,
-            postcode: this.customerDetails.billingAddress.postcode
-          });
+          this.pathDetailsForm();
+          this.patchShippingAddressForm();
+          this.patchBillingAddressForm();
         }
       )
     }
+  }
 
-    this.cartSub = this.cartService.getCart().subscribe(
-      (response) => {
-        console.log(response);
-        this.cartDetails = response;
-      }, (error) => {
-        console.log(error);
-        this.loading = false;
-      },
-      () => {
-        this.loading = false;
-      }
-    );
+  private pathDetailsForm() {
+    this.detailsForm.patchValue({
+      firstName: this.customerDetails.firstName,
+      lastName: this.customerDetails.lastName,
+      email: this.customerDetails.email,
+      phoneNumber: this.customerDetails.phoneNumber
+    });
+  }
+
+  private patchShippingAddressForm() {
+    this.shippingAddressForm.patchValue({
+      id: this.customerDetails.shippingAddress.id,
+      city: this.customerDetails.shippingAddress.city,
+      street: this.customerDetails.shippingAddress.street,
+      number: this.customerDetails.shippingAddress.number,
+      floor: this.customerDetails.shippingAddress.floor,
+      door: this.customerDetails.shippingAddress.door,
+      country: this.customerDetails.shippingAddress.country,
+      postcode: this.customerDetails.shippingAddress.postcode
+    });
+  }
+
+  private patchBillingAddressForm() {
+    this.billingAddressForm.patchValue({
+      id: this.customerDetails.billingAddress.id,
+      city: this.customerDetails.billingAddress.city,
+      street: this.customerDetails.billingAddress.street,
+      number: this.customerDetails.billingAddress.number,
+      floor: this.customerDetails.billingAddress.floor,
+      door: this.customerDetails.billingAddress.door,
+      country: this.customerDetails.billingAddress.country,
+      postcode: this.customerDetails.billingAddress.postcode
+    });
   }
 
   copyShippingDetails() {
@@ -149,13 +183,14 @@ export class CheckoutComponent implements OnInit, OnDestroy {
     this.orderRequest = this.detailsForm.value;
     this.orderRequest.shippingAddress = this.shippingAddressForm.value;
     this.orderRequest.billingAddress = this.billingAddressForm.value;
-    this.orderRequest.paymentMethod = "BANK_CARD";
+    this.orderRequest.paymentMethod = this.chosenPaymentMethod.paymentMethod;
 
     this.createOrderSub = this.orderService.createOrder(this.orderRequest).subscribe(
       (response) => {
         console.log(response);
       }, (error) => {
         console.log(error);
+        errorHandler(error, this.detailsForm);
         this.loading = false;
       },
       () => {
@@ -167,6 +202,7 @@ export class CheckoutComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     this.cartSub.unsubscribe();
+    this.paymentMethodsSub.unsubscribe();
     if (this.customerDetailsSub) {
       this.customerDetailsSub.unsubscribe();
     }

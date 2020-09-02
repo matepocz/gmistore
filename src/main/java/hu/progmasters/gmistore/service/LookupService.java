b1afb0f9de3c504c1c84854c107945ff748259a8
pocz.mate <mate.pocz@gmail.com>
2020-09-02 22:13:23 +0200

    add getPaymentMethods

--- src/main/java/hu/progmasters/gmistore/service/LookupService.java
+++ src/main/java/hu/progmasters/gmistore/service/LookupService.java
@@ -1,5 +1,6 @@
 package hu.progmasters.gmistore.service;
 
+import hu.progmasters.gmistore.dto.PaymentMethodDetails;
 import hu.progmasters.gmistore.enums.DomainType;
 import hu.progmasters.gmistore.model.LookupEntity;
 import hu.progmasters.gmistore.repository.LookupRepository;
@@ -7,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -21,6 +24,14 @@ public class LookupService {
 
     public LookupEntity getPaymentMethodByKey(String key) {
         return lookupRepository.findByDomainTypeAndLookupKey(DomainType.PAYMENT_METHOD, key);
+    }
+
+    public List<PaymentMethodDetails> getPaymentMethods() {
+        List<LookupEntity> paymentMethods = lookupRepository.findByDomainType(DomainType.PAYMENT_METHOD);
+        return paymentMethods.stream()
+                .map(paymentMethod -> new PaymentMethodDetails(
+                        paymentMethod.getId(), paymentMethod.getLookupKey(), paymentMethod.getDisplayName()))
+                .collect(Collectors.toList());
     }
 
     public LookupEntity getOrderStatusByKey(String key) {

ca3c76a645a5ad2694f2b93debe51d5158d216f1
pocz.mate <mate.pocz@gmail.com>
2020-09-07 22:26:27 +0200

    add getUserRoles endpoint

--- src/main/java/hu/progmasters/gmistore/controller/AuthController.java
+++ src/main/java/hu/progmasters/gmistore/controller/AuthController.java
@@ -1,23 +1,32 @@
 package hu.progmasters.gmistore.controller;
 
+import hu.progmasters.gmistore.dto.AuthenticationResponse;
 import hu.progmasters.gmistore.dto.LoginRequest;
 import hu.progmasters.gmistore.dto.RegisterRequest;
 import hu.progmasters.gmistore.model.User;
-import hu.progmasters.gmistore.response.AuthenticationResponse;
 import hu.progmasters.gmistore.response.ConfirmAccountResponse;
 import hu.progmasters.gmistore.service.AuthService;
 import hu.progmasters.gmistore.validator.RegisterRequestValidator;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
+
+    private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(AuthController.class);
+
 
     private final AuthService authService;
     private final RegisterRequestValidator registerRequestValidator;
@@ -49,22 +58,29 @@ public class AuthController {
 
     @PutMapping("/login")
     public ResponseEntity<AuthenticationResponse> login(@RequestBody LoginRequest loginRequest) {
-        AuthenticationResponse response = new AuthenticationResponse(
-                authService.login(loginRequest),
-                loginRequest.getUsername()
-        );
-        return new ResponseEntity<>(response, HttpStatus.OK);
+        AuthenticationResponse loginResponse = authService.login(loginRequest);
+        return new ResponseEntity<>(loginResponse, HttpStatus.OK);
     }
 
     @GetMapping("/check-username-taken/{username}")
     public ResponseEntity<Boolean> checkIfUsernameTaken(@PathVariable("username") String username) {
         boolean result = authService.checkIfUsernameExists(username);
-        return new ResponseEntity<>(result,HttpStatus.OK);
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
     @GetMapping("/check-email-taken/{email}")
     public ResponseEntity<Boolean> checkIfEmailTaken(@PathVariable("email") String email) {
         boolean result = authService.checkIfEmailExists(email);
-        return new ResponseEntity<>(result,HttpStatus.OK);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    @GetMapping("/user-roles")
+    public ResponseEntity<List<String>> currentUserRoles() {
+        Collection<? extends GrantedAuthority> authorities =
+                SecurityContextHolder.getContext().getAuthentication().getAuthorities();
+        List<String> roles = authorities.stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList());
+        return !roles.isEmpty() ?
+                new ResponseEntity<>(roles, HttpStatus.OK) :
+                new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
     }
 }

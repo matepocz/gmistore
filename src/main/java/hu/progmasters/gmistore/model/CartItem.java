cf73c61872384c8b21a743529bcdafa1d397a43f
pocz.mate <mate.pocz@gmail.com>
2020-09-09 22:34:42 +0200

    add constraints

--- src/main/java/hu/progmasters/gmistore/model/CartItem.java
+++ src/main/java/hu/progmasters/gmistore/model/CartItem.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
@@ -18,10 +19,12 @@ public class CartItem {
     private Long id;
 
     @ManyToOne(targetEntity = Product.class)
+    @NotNull
     @JoinColumn(name = "product_id", referencedColumnName = "id")
     private Product product;
 
     @Column(name = "count")
+    @NotNull
     @Min(value = 1, message = "Minimum 1 pc of this product!")
     private Integer count;
 }

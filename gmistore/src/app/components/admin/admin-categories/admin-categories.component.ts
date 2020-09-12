781451fefba392908c34edcfd6724779519b342f
pocz.mate <mate.pocz@gmail.com>
2020-09-12 22:10:43 +0200

    create Main category works

--- gmistore/src/app/components/admin/admin-categories/admin-categories.component.ts
+++ gmistore/src/app/components/admin/admin-categories/admin-categories.component.ts
@@ -1,9 +1,11 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {AdminService} from "../../../service/admin.service";
 import {MainCategoryModel} from "../../../models/main-category.model";
 import {Subscription} from "rxjs";
-import {FormBuilder, Validators} from "@angular/forms";
+import {FormBuilder, FormGroupDirective, Validators} from "@angular/forms";
 import {NewMainCategoryModel} from "../../../models/new-main-category.model";
+import {errorHandler} from "../../../utils/error-handler";
+import {MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-admin-categories',
@@ -11,6 +13,11 @@ import {NewMainCategoryModel} from "../../../models/new-main-category.model";
   styleUrls: ['./admin-categories.component.css']
 })
 export class AdminCategoriesComponent implements OnInit, OnDestroy {
+
+  @ViewChild(FormGroupDirective) mainCategoryDirective: FormGroupDirective;
+
+  horizontalPosition: MatSnackBarHorizontalPosition = 'center';
+  verticalPosition: MatSnackBarVerticalPosition = 'bottom';
 
   categories: Array<MainCategoryModel>;
 
@@ -25,11 +32,17 @@ export class AdminCategoriesComponent implements OnInit, OnDestroy {
   });
 
   categoriesSub: Subscription;
+  createMainCategorySub: Subscription;
 
-  constructor(private adminService: AdminService, private formBuilder: FormBuilder) {
+  constructor(private adminService: AdminService, private formBuilder: FormBuilder,
+              private snackBar: MatSnackBar) {
   }
 
   ngOnInit(): void {
+    this.fetchCategories();
+  }
+
+  fetchCategories() {
     this.categoriesSub = this.adminService.getProductCategories().subscribe(
       (response: Array<MainCategoryModel>) => {
         this.categories = response;
@@ -41,9 +54,34 @@ export class AdminCategoriesComponent implements OnInit, OnDestroy {
 
   onCategorySubmit() {
     this.newMainCategory = this.categoryForm.value;
+    this.adminService.createMainCategory(this.newMainCategory).subscribe(
+      (response) => {
+        if (response) {
+          this.openSnackBar("Kategória hozzáadva!");
+          this.fetchCategories();
+          this.mainCategoryDirective.resetForm();
+        } else {
+          this.openSnackBar("Valami hiba történt!");
+        }
+      }, (error) => {
+        console.log(error);
+        errorHandler(error, this.categoryForm);
+      }
+    )
+  }
+
+  openSnackBar(message: string) {
+    this.snackBar.open(message, 'OK', {
+      duration: 2000,
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+    });
   }
 
   ngOnDestroy() {
     this.categoriesSub.unsubscribe();
+    if (this.createMainCategorySub) {
+      this.createMainCategorySub.unsubscribe();
+    }
   }
 }

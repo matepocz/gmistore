1dd7375c345ffb097e56f08af84a8e41e7ab7f09
kristof.csef@gmail.com <kristof.csef@gmail.com>
2020-09-15 22:59:22 +0200

    profile edit changed

--- gmistore/src/app/components/user/user-profile/user-profile.component.ts
+++ gmistore/src/app/components/user/user-profile/user-profile.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewChecked, ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {UserService} from "../../../service/user.service";
 import {UserModel} from "../../../models/user-model";
 import {AddressModel} from "../../../models/address-model";
@@ -6,56 +6,107 @@ import {HttpClient} from "@angular/common/http";
 import {ActivatedRoute, Router} from "@angular/router";
 import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {OrderModel} from "../../../models/order-model";
+import {RolesInitModel} from "../../../models/rolesInitModel";
+import {SharingService} from "../../../service/sharing.service";
+import {AdminService} from "../../../service/admin.service";
+import {UserEditableDetailsByAdmin} from "../../../models/userEditableDetailsByAdmin";
+import {errorHandler} from "../../../utils/error-handler";
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.css']
 })
-export class UserProfileComponent implements OnInit {
-  user: UserModel;
+export class UserProfileComponent implements OnInit , AfterViewChecked{
+  userForm: FormGroup;
   details: any[];
-  shippingAddress: any[];
-  billingAddress: any[];
-  titles = ['Keresztnév', 'Vezetéknév', 'Felhasználónév', 'Email', 'Telefon', 'Regisztrált'];
-  address = ['Város', 'Utca', 'Emelet', 'Irányítószám', 'Ország'];
+  user: UserModel;
+  loaded: boolean = false;
+  roleOptions: Array<RolesInitModel>;
+  toSend: any;
 
+  constructor(private sharingService: SharingService,
+              private fb: FormBuilder, private route: ActivatedRoute,
+              private adminService: AdminService,
+              private router: Router, private cdRef: ChangeDetectorRef) {
 
-  constructor(private userService: UserService) {
+    this.userForm = this.fb.group({
+      shippingAddress: this.fb.group({
+        city: [''],
+        street: [''],
+        number: ['',Validators.pattern("^[0-9]*$")],
+        floor: [''],
+        door: ['',Validators.pattern("^[0-9]*$")],
+        country: [''],
+        postcode: ['',Validators.pattern("^[0-9]*$")]
+      }),
+      billingAddress: this.fb.group({
+        city: [''],
+        street: [''],
+        number: ['',Validators.pattern("^[0-9]*$")],
+        floor: ['',Validators.pattern("^[0-9]*$")],
+        door: ['',Validators.pattern("^[0-9]*$")],
+        country: [''],
+        postcode: ['',Validators.pattern("^[0-9]*$")]
+      }),
+      username: ['', Validators.required],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.required],
+      phoneNumber: [''],
+      registered: ['', Validators.required],
+      active: ['', Validators.required],
+
+    })
   }
+
 
   ngOnInit(): void {
-    this.getUserDetails();
+    this.route.paramMap.subscribe(
+      paramMap => {
+        const editableId = paramMap.get('id');
+        if (editableId) {
+          this.getUserDetails(editableId);
+        }
+      },
+      error => console.warn(error),
+    );
   }
 
-  getUserDetails() {
-    this.userService.getUser().subscribe(data => {
-      console.log(data)
-      this.user = data;
-      this.details = [data.firstName, data.lastName, data.username, data.email, data.phoneNumber, this.getDate(data.registered)]
-
-      this.shippingAddress = [data.shippingAddress.city, data.shippingAddress.street, data.shippingAddress.floor
-        , data.shippingAddress.postcode, data.shippingAddress.country];
-
-      this.billingAddress = [data.billingAddress.city, data.billingAddress.street, data.billingAddress.floor
-        , data.billingAddress.postcode, data.billingAddress.country];
-
-    }, error => console.log(error))
+  ngAfterViewChecked() {
+    this.cdRef.detectChanges();
   }
 
-  getDate = (d: Date) => {
-    let dt = new Date(d);
-    let dtm = dt.getMonth();
-    let dty = dt.getFullYear();
-    let day = dt.getDay();
-    return dty + "/" + dtm + "/" + day
+  getUserDetails(id) {
+    this.adminService.getInitRoles().subscribe(
+      rolesData => {
+        this.roleOptions = rolesData
+      })
+
+    this.adminService.getAccount(id).subscribe(
+      data => {
+        console.log(data);
+        this.user = data;
+        this.sharingService.nextMessage(data);
+      },
+      error => console.log(error),
+      () => this.loaded = true
+    )
   }
 
-  getDate2(d: Date): any {
-    let dt = new Date(d);
-    let dtm = dt.getMonth();
-    let dty = dt.getFullYear();
-    return dtm + "/" + dty
+  onSubmit(id:number) {
+    const data: UserEditableDetailsByAdmin = {...this.userForm.value};
+    data.roles = this.toSend;
+    data.id = id;
+    this.updateUser(data);
+  }
+
+  private updateUser(data: UserEditableDetailsByAdmin) {
+    console.log(data)
+    this.adminService.updateUser(data).subscribe(
+      () => this.router.navigate(['/admin/user']),
+      error => errorHandler(error, this.userForm),
+    );
   }
 }
 

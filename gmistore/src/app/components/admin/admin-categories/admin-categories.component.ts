41b8c3bd25cdc4a012f889f3ec6f9b74ca6c4c74
pocz.mate <mate.pocz@gmail.com>
2020-09-16 16:45:59 +0200

    impl delete category (set to inactive)

--- gmistore/src/app/components/admin/admin-categories/admin-categories.component.ts
+++ gmistore/src/app/components/admin/admin-categories/admin-categories.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {Component, Inject, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {AdminService} from "../../../service/admin.service";
 import {MainCategoryModel} from "../../../models/main-category.model";
 import {Subscription} from "rxjs";
@@ -6,12 +6,35 @@ import {FormBuilder, FormGroupDirective, Validators} from "@angular/forms";
 import {NewCategoryModel} from "../../../models/new-category.model";
 import {errorHandler} from "../../../utils/error-handler";
 import {MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition} from "@angular/material/snack-bar";
+import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from "@angular/material/dialog";
+
+export interface DialogData {
+  name: string;
+  key: string;
+}
+
+@Component({
+  selector: 'delete-dialog',
+  templateUrl: 'delete-dialog.html',
+})
+
+export class DeleteDialog {
+  constructor(
+    public dialogRef: MatDialogRef<DeleteDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+}
 
 @Component({
   selector: 'app-admin-categories',
   templateUrl: './admin-categories.component.html',
   styleUrls: ['./admin-categories.component.css']
 })
+
 export class AdminCategoriesComponent implements OnInit, OnDestroy {
 
   @ViewChild(FormGroupDirective) categoryForm: FormGroupDirective;
@@ -47,7 +70,7 @@ export class AdminCategoriesComponent implements OnInit, OnDestroy {
   createSubCategorySub: Subscription;
 
   constructor(private adminService: AdminService, private formBuilder: FormBuilder,
-              private snackBar: MatSnackBar) {
+              private snackBar: MatSnackBar, public dialog: MatDialog) {
   }
 
   ngOnInit(): void {
@@ -102,11 +125,39 @@ export class AdminCategoriesComponent implements OnInit, OnDestroy {
     )
   }
 
+  setCategoryInactive(key: string) {
+    this.adminService.setCategoryInactive(key).subscribe(
+      (response) => {
+        if (response) {
+          this.openSnackBar("Kategória inaktív!")
+          this.fetchCategories();
+        } else {
+          this.openSnackBar("Nincs jogosultságod a művelethez!")
+        }
+      }, () => {
+        this.openSnackBar("Valami hiba történt!");
+      }
+    )
+  }
+
   openSnackBar(message: string) {
     this.snackBar.open(message, 'OK', {
       duration: 2000,
       horizontalPosition: this.horizontalPosition,
       verticalPosition: this.verticalPosition,
+    });
+  }
+
+  openDialog(category: string, key: string): void {
+    const dialogRef = this.dialog.open(DeleteDialog, {
+      width: '250px',
+      data: {name: category}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.setCategoryInactive(key);
+      }
     });
   }
 

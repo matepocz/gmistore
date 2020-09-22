8d7fdb920a3c41387192cf1156555745d16e3bad
pocz.mate <mate.pocz@gmail.com>
2020-09-22 23:06:29 +0200

    extend JpaSpecificationExecutor

--- src/main/java/hu/progmasters/gmistore/repository/ProductRepository.java
+++ src/main/java/hu/progmasters/gmistore/repository/ProductRepository.java
@@ -5,6 +5,7 @@ import hu.progmasters.gmistore.model.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -12,7 +13,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
     Optional<Product> findProductByName(String name);
 
     Optional<Product> findProductById(Long id);

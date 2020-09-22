92c566bb861eb678efab65fd3718c6d5a428f255
pocz.mate <mate.pocz@gmail.com>
2020-09-22 23:22:53 +0200

    WEBSHOP01-91 & WEBSHOP01-121 fixed

---
+++ src/main/java/hu/progmasters/gmistore/service/CartItemComparator.java
@@ -0,0 +1,13 @@
+package hu.progmasters.gmistore.service;
+
+import hu.progmasters.gmistore.dto.CartItemDto;
+
+import java.util.Comparator;
+
+public class CartItemComparator implements Comparator<CartItemDto> {
+
+    @Override
+    public int compare(CartItemDto cartItemDtoFirst, CartItemDto cartItemDtoSecond) {
+        return Long.compare(cartItemDtoFirst.getId(), cartItemDtoSecond.getId());
+    }
+}

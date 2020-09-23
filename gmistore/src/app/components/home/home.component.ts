29e371c56795120386a5fbfac2c5f8b02678f01a
palasti_imre <palastiimre@gmail.com>
2020-09-23 20:56:28 +0200

    impl spinner to home.component.ts

--- gmistore/src/app/components/home/home.component.ts
+++ gmistore/src/app/components/home/home.component.ts
@@ -1,17 +1,20 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {OwlOptions} from 'ngx-owl-carousel-o';
 import {ProductService} from "../../service/product-service";
 import {Title} from "@angular/platform-browser";
 import {ProductModel} from "../../models/product-model";
 import {Router} from "@angular/router";
-
+import {Subscription} from "rxjs";
+import {SpinnerService} from "../../service/spinner-service.service";
+import {MatDialogRef} from "@angular/material/dialog";
+import {LoadingSpinnerComponent} from "../loading-spinner/loading-spinner.component";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   customOptions: OwlOptions = {
     loop: true,
     autoplay: true,
@@ -58,23 +61,36 @@ export class HomeComponent implements OnInit {
 
 
   products: Array<ProductModel>;
+  private productSub: Subscription;
+  spinner: MatDialogRef<LoadingSpinnerComponent> = this.spinnerService.start();
 
-  constructor(private productService: ProductService, private titleService: Title,private router:Router) {
-    this.productService.getDiscountProducts().subscribe(
-      (response) => {
-        this.products = response;
-      }, error => {
-      }, () => {
-
-      }
-    );
+  constructor(private productService: ProductService,
+              private titleService: Title,
+              private router: Router,
+              private spinnerService: SpinnerService) {
   }
 
   ngOnInit(): void {
+    this.productSub = this.productService.getDiscountProducts().subscribe(
+      (response) => {
+        this.products = response;
+        this.spinnerService.stop(this.spinner);
+      }, error => {
+        this.spinnerService.stop(this.spinner);
+      }, () => {
+        this.spinnerService.stop(this.spinner);
+      }
+    );
     this.titleService.setTitle("Főoldal - GMI Store");
   }
 
   navigateToProduct(slug: string) {
-    this.router.navigate(['/product/'+slug]);
+    this.router.navigate(['/product/' + slug]);
   }
+
+  ngOnDestroy(): void {
+    this.productSub.unsubscribe();
+  }
+
+
 }

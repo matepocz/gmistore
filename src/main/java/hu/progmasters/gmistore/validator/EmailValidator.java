958c7081672ed4e14ec2f5f0ffbbe0068204b1bd
palasti_imre <palastiimre@gmail.com>
2020-09-23 23:18:58 +0200

    WEBSHOP01-128 bug fix WEBSHOP01-127 bug fix

--- src/main/java/hu/progmasters/gmistore/validator/EmailValidator.java
+++ src/main/java/hu/progmasters/gmistore/validator/EmailValidator.java
@@ -19,15 +19,30 @@ public class EmailValidator implements Validator {
 
 
         String email = emailCreating.getEmail();
+        String subject = emailCreating.getSubject();
+        String message = emailCreating.getMessage();
+
         if (email == null || email.isEmpty()) {
             errors.rejectValue("email", "email.empty");
         } else if (!email.matches("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")) {
             errors.rejectValue("email", "email.invalid");
         }
 
-        String subject = emailCreating.getSubject();
         if (subject == null || subject.isEmpty()) {
             errors.rejectValue("subject", "subject.is-empty");
+        } else if (subject.length() < 3) {
+            errors.rejectValue("subject", "subject.short");
+        } else if (subject.length() > 30) {
+            errors.rejectValue("subject", "subject.tooLong");
+        }
+
+
+        if (message == null || message.isEmpty()) {
+            errors.rejectValue("message", "message.is-empty");
+        } else if (message.length() < 3) {
+            errors.rejectValue("message", "message.short");
+        } else if (message.length() > 1000) {
+            errors.rejectValue("message", "message.tooLong");
         }
     }
 }

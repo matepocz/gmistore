d553f4dcf94b617503a07615d5125a84c0d2d1b0
kristof.csef@gmail.com <kristof.csef@gmail.com>
2020-09-24 13:41:19 +0200

    minor improvements

--- src/main/java/hu/progmasters/gmistore/model/Order.java
+++ src/main/java/hu/progmasters/gmistore/model/Order.java
@@ -47,7 +47,6 @@ public class Order implements Serializable {
 
     @ManyToOne
     @JoinColumn(name = "shipping_method", referencedColumnName = "id")
-    @JsonIgnore
     private ShippingMethod shippingMethod;
 
     @ManyToOne

7761656388eef2c3b53c760ec7107f556ea10348
pocz.mate <mate.pocz@gmail.com>
2020-09-30 13:29:43 +0200

    improve validator

--- src/main/java/hu/progmasters/gmistore/validator/RatingRequestValidator.java
+++ src/main/java/hu/progmasters/gmistore/validator/RatingRequestValidator.java
@@ -20,5 +20,25 @@ public class RatingRequestValidator implements Validator {
         if (ratingRequest.getActualRating() == null) {
             errors.rejectValue("actualRating", "rating.emptyRating");
         }
+
+        if (ratingRequest.getTitle() != null && ratingRequest.getTitle().length() > 100) {
+            errors.rejectValue("title", "rating.title.tooLong");
+        }
+
+        if (ratingRequest.getPositiveComment() == null) {
+            errors.rejectValue("positiveComment", "rating.emptyRating");
+        } else if (ratingRequest.getPositiveComment().length() < 3) {
+            errors.rejectValue("positiveComment", "rating.tooShort");
+        } else if (ratingRequest.getPositiveComment().length() > 1000) {
+            errors.rejectValue("positiveComment", "rating.tooLong");
+        }
+
+        if (ratingRequest.getNegativeComment() == null) {
+            errors.rejectValue("positiveComment", "rating.emptyRating");
+        } else if (ratingRequest.getNegativeComment().length() < 3) {
+            errors.rejectValue("positiveComment", "rating.tooShort");
+        } else if (ratingRequest.getNegativeComment().length() > 1000) {
+            errors.rejectValue("positiveComment", "rating.tooLong");
+        }
     }
 }

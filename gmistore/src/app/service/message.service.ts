1b58bc29804ea38e371dc9c28f6011ebcd5e55a3
pocz.mate <mate.pocz@gmail.com>
2020-10-03 22:29:00 +0200

    add methods

--- gmistore/src/app/service/message.service.ts
+++ gmistore/src/app/service/message.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {environment} from "../../environments/environment";
+import {MessagesResponseModel} from "../models/messages/messages-response.model";
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +11,18 @@ export class MessageService {
 
   private messagesApi = environment.apiUrl + 'api/messages';
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {
+  }
 
   getCountOfUnreadMails(): Observable<number> {
     return this.httpClient.get<number>(this.messagesApi + '/unread-mail-count');
+  }
+
+  getMessages(): Observable<MessagesResponseModel> {
+    return this.httpClient.get<MessagesResponseModel>(this.messagesApi);
+  }
+
+  markMessageRead(id: number): Observable<boolean> {
+    return this.httpClient.put<boolean>(this.messagesApi + '/mark-read/' + id, {});
   }
 }

cddcf7507f6a5ac73e6964894851ebcb9a92f835
pocz.mate <mate.pocz@gmail.com>
2020-10-08 22:09:33 +0200

    more tests on OrderService

--- src/test/java/hu/progmasters/gmistore/service/OrderServiceTest.java
+++ src/test/java/hu/progmasters/gmistore/service/OrderServiceTest.java
@@ -1,8 +1,10 @@
 package hu.progmasters.gmistore.service;
 
+import hu.progmasters.gmistore.dto.AddressDetails;
 import hu.progmasters.gmistore.dto.CustomerDetails;
-import hu.progmasters.gmistore.model.Address;
-import hu.progmasters.gmistore.model.User;
+import hu.progmasters.gmistore.dto.order.OrderRequest;
+import hu.progmasters.gmistore.enums.DomainType;
+import hu.progmasters.gmistore.model.*;
 import hu.progmasters.gmistore.repository.OrderRepository;
 import hu.progmasters.gmistore.repository.OrderStatusHistoryRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,7 +13,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import javax.servlet.http.HttpSession;
 import java.security.Principal;
+import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.function.Supplier;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -49,6 +54,9 @@ public class OrderServiceTest {
     @Mock
     private Principal principalMock;
 
+    @Mock
+    private HttpSession session;
+
     private final Supplier<Address> addressSupplier = () -> {
         Address address = new Address();
         address.setId(1L);
@@ -72,6 +80,63 @@ public class OrderServiceTest {
         user.setPhoneNumber("+36201234567");
         user.setEmail("john.doe@gmail.com");
         return user;
+    };
+
+    private final Supplier<Cart> cartSupplier = () -> {
+        Cart cart = new Cart();
+        cart.setId(1L);
+        cart.setItems(new HashSet<>());
+        cart.setUser(userSupplier.get());
+        cart.setItemsTotalPrice(1000.0);
+        cart.setTotalPrice(1100.0);
+        return cart;
+    };
+
+    private final Supplier<Product> productSupplier = () -> {
+        LookupEntity mainCategory = new LookupEntity();
+        mainCategory.setId(1L);
+        mainCategory.setDisplayFlag(true);
+        mainCategory.setLookupKey("main_category");
+        mainCategory.setDisplayName("Main Category");
+        mainCategory.setDomainType(DomainType.PRODUCT_CATEGORY);
+
+        LookupEntity subCategory = new LookupEntity();
+        subCategory.setId(2L);
+        subCategory.setDisplayFlag(true);
+        subCategory.setLookupKey("sub_category");
+        subCategory.setDisplayName("Sub Category");
+        subCategory.setDomainType(DomainType.PRODUCT_CATEGORY);
+        subCategory.setParent(mainCategory);
+
+        Product product = new Product();
+
+        Inventory inventory = new Inventory();
+        inventory.setQuantityAvailable(1);
+        inventory.setId(1L);
+        inventory.setProduct(product);
+
+        product.setId(1L);
+        product.setName("test product");
+        product.setProductCode("GMI123");
+        product.setSlug("test-product-gmi123");
+        product.setDescription("test 1");
+        product.setMainCategory(mainCategory);
+        product.setSubCategory(subCategory);
+        product.setActive(true);
+        product.setPrice(100.0);
+        product.setDiscount(0);
+        product.setWarrantyMonths(12);
+        product.setAddedBy("unknown");
+        product.setInventory(inventory);
+        return product;
+    };
+
+    private final Supplier<CartItem> cartItemSupplier = () -> {
+        CartItem cartItem = new CartItem();
+        cartItem.setId(1L);
+        cartItem.setCount(1);
+        cartItem.setProduct(productSupplier.get());
+        return cartItem;
     };
 
     @BeforeEach
@@ -110,5 +175,62 @@ public class OrderServiceTest {
         assertEquals("john.doe@gmail.com", user.getEmail());
         verify(principalMock, times(1)).getName();
         verify(userServiceMock, times(1)).getUserByUsername("username");
+    }
+
+    @Test
+    public void testCreateOrderCartIsEmptyShouldReturnFalse() {
+        Cart cart = cartSupplier.get();
+        OrderRequest orderRequest = new OrderRequest();
+
+        when(cartServiceMock.getActualCart(any())).thenReturn(cart);
+
+        boolean result = orderService.createOrder(orderRequest, session, principalMock);
+
+        assertFalse(result);
+    }
+
+    @Test
+    public void testCreateOrderSuccessfully() {
+        Address address = addressSupplier.get();
+        AddressDetails shippingAddress = new AddressDetails(address);
+
+        OrderRequest orderRequest = new OrderRequest();
+        orderRequest.setEmail("john.doe@gmail.com");
+        orderRequest.setFirstName("John");
+        orderRequest.setLastName("Doe");
+        orderRequest.setPhoneNumber("+36201234567");
+        orderRequest.setPaymentMethod("test");
+        orderRequest.setShippingAddress(shippingAddress);
+        orderRequest.setBillingAddress(shippingAddress);
+
+        Cart cart = cartSupplier.get();
+        cart.getItems().add(cartItemSupplier.get());
+        cart.setExpectedDeliveryDate(LocalDateTime.now());
+
+        ShippingMethod shippingMethod = new ShippingMethod();
+        shippingMethod.setId(1);
+        shippingMethod.setCost(100);
+        shippingMethod.setMethod("test");
+        shippingMethod.setDays(2);
+
+        cart.setShippingMethod(shippingMethod);
+
+        User user = userSupplier.get();
+
+        LookupEntity paymentMethod = new LookupEntity();
+        paymentMethod.setId(1L);
+        paymentMethod.setLookupKey("test");
+        paymentMethod.setDomainType(DomainType.PAYMENT_METHOD);
+        paymentMethod.setDisplayFlag(true);
+        paymentMethod.setDisplayName("test");
+
+        when(cartServiceMock.getActualCart(session)).thenReturn(cart);
+        when(principalMock.getName()).thenReturn("john");
+        when(userServiceMock.getUserByUsername("john")).thenReturn(user);
+        when(lookupServiceMock.getPaymentMethodByKey("test")).thenReturn(paymentMethod);
+
+        boolean result = orderService.createOrder(orderRequest, session, principalMock);
+
+        assertTrue(result);
     }
 }
